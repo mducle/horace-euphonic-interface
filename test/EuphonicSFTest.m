09ab2ece48c31b8d75ab1b18164a59b761188a45
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-06-17 16:17:37 +0100

    Add more test parameterisation

--- test/EuphonicSFTest.m
+++ test/EuphonicSFTest.m
@@ -5,24 +5,34 @@ classdef EuphonicSFTest < matlab.unittest.TestCase
         pars
         scattering_lengths
         opts
+        material_name
     end
 
     properties (ClassSetupParameter)
-        material_opts = {{'model', 'CASTEP', ...
-                          'model_args', {get_abspath('quartz.castep_bin')}}, ...
-                         {'model', 'CASTEP', ...
-                          'model_args', {get_abspath('La2Zr2O7.castep_bin')}}, ...
-                         {'model', 'phonopy', ...
-                          'model_kwargs' {'path', get_abspath('NaCl')}}};
+        temp = {300};
+        materials = { ...
+           {'quartz', {'model', 'CASTEP', ...
+                       'model_args', {get_abspath('quartz.castep_bin', 'input')}}}, ...
+          {'nacl', {'model', 'phonopy', ...
+                    'model_kwargs' {'path', get_abspath('NaCl', 'input')}}}};
+        dw_grid = {missing, [6,6,6]};
+        bose = {missing, false};
+        negative_e = {missing, true};
+        conversion_mat =  {missing, (1/2)*[-1, 1, 1; 1, -1, 1; 1, 1, -1]};
+        lim = {missing, 1e-9};
     end
 
+    % These parameters are related to performance, and shouldn't change the
+    % result
     properties (TestParameter)
         use_c = {false, true, true};
         n_threads = {int32(1), int32(1), int32(2)};
+        chunk = {2, missing, missing};
     end
 
     methods (TestClassSetup)
-        function setQpts(testCase, material_opts)
+        function setQpts(testCase, temp, materials, dw_grid, bose, ...
+                         negative_e, conversion_mat, lim)
             qpts = [0.0, 0.0, 0.0;
                     0.1, 0.2, 0.3;
                     0.4, 0.5, 0.0;
@@ -31,27 +41,48 @@ classdef EuphonicSFTest < matlab.unittest.TestCase
             scattering_lengths = struct('La', 8.24, 'Zr', 7.16, 'O', 5.803, ...
                                         'Si', 4.1491, 'Na', 3.63, 'Cl', 9.577);
             scale = 1.0;
-            temp = 5;
+
+            opts = materials{2};
+            % Only add values to opts if they aren't missing
+            opts_keys = {'dw_grid', 'bose', 'negative_e', 'conversion_mat', ...
+                         'lim'};
+            opts_values = {dw_grid, bose, negative_e, conversion_mat, lim};
+            for i=1:length(opts_keys)
+                if ~ismissing(opts_values{i})
+                    opts = [opts {opts_keys{i}, opts_values{i}}];
+                end
+            end
 
             testCase.qpts = qpts;
             testCase.pars = [temp scale];
             testCase.scattering_lengths = scattering_lengths;
-            testCase.opts = material_opts;
+            testCase.opts = opts;
+            testCase.material_name = materials{1};
         end
     end
 
     methods(Test, ParameterCombination='sequential')
-        function testThing(testCase, use_c, n_threads)
+        function runTests(testCase, use_c, n_threads, chunk)
+            run_tests = true;
+            testCase.assumeEqual(run_tests, true);
+
             qpts = testCase.qpts;
             opts = testCase.opts;
+            pars = testCase.pars;
             phonon_kwargs = {'phonon_kwargs', ...
                                 {'asr', 'reciprocal', ...
                                  'use_c', use_c, ...
                                  'n_threads', n_threads}};
             opts = [opts phonon_kwargs];
+            if ~ismissing(chunk)
+                opts = [opts {'chunk', chunk}];
+            end
+            fname = get_expected_output_filename(testCase.material_name, ...
+                                                 pars, opts);
+
             [w, sf] = euphonic_sf(qpts(:, 1), qpts(:, 2), qpts(:, 3), ...
-                                  testCase.pars, ...
-                                  testCase.scattering_lengths, opts);
+                                  pars, testCase.scattering_lengths, opts);
+            disp(fname);
         end
     end
 
@@ -59,7 +90,18 @@ classdef EuphonicSFTest < matlab.unittest.TestCase
         function generateTestData(testCase)
             generate_test_data = false;
             testCase.assumeEqual(generate_test_data, true);
-            disp('Generating test data...')
+
+            qpts = testCase.qpts;
+            opts = testCase.opts;
+            pars = testCase.pars;
+            phonon_kwargs = {'phonon_kwargs', {'asr', 'reciprocal'}};
+            opts = [opts phonon_kwargs];
+            [w, sf] = euphonic_sf(qpts(:,1), qpts(:,2), qpts(:,3), ...
+                                  testCase.pars, testCase.scattering_lengths, ...
+                                  opts)
+            fname = get_expected_output_filename(testCase.material_name, ...
+                                                 pars, opts);
+            save(get_abspath(fname, 'expected_output'), 'w', 'sf');
         end
     end
 end
--- test/get_abspath.m
+++ test/get_abspath.m
@@ -1,6 +1,6 @@
-function abspath = get_abspath(filename)
+function abspath = get_abspath(filename, sub_dir)
     % Get absolute path to a file in the test directory
     s = what('test');
     test_dir = s.path;
-    abspath =  [test_dir filesep 'input' filesep filename];
+    abspath =  [test_dir filesep sub_dir filesep filename];
 end
