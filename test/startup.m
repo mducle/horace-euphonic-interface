e67189a248543c1b38e68bc1362afb85575fbe45
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-06-19 16:34:06 +0100

    Update startup.m for Windows

--- test/startup.m
+++ test/startup.m
@@ -1,4 +1,8 @@
 addpath('..')
-pyversion '/home/jenkins/.conda/envs/py/bin/python'
-py.sys.setdlopenflags(int32(10))
+if ispc
+    pyversion 'C:\Programming\miniconda3\envs\py\python'
+else
+    pyversion '/home/jenkins/.conda/envs/py/bin/python'
+    py.sys.setdlopenflags(int32(10))
+end
 euphonic_on
