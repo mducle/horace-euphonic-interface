2b79454ff03e0945e56a9cf04015fb34683aff6a
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-11-27 10:58:26 +0000

    Add Euphonic version verification

--- euphonic_on.m
+++ euphonic_on.m
@@ -3,13 +3,16 @@ function euphonic_on()
 % Set number of openblas threads to 1 so it doesn't interfere with openmp
 setenv('OPENBLAS_NUM_THREADS', '1');
 
+% Check if Euphonic is installed and the correct version
+verify_python_modules('euphonic', '0.3.2')
+
 % Check if Euphonic can be loaded
 try
     py.importlib.import_module('euphonic');
 catch ME
     [ver, ex, isloaded] = pyversion;
-    warning(['Couldn''t import Euphonic Python library. Does the ' ...
-             'currently loaded Python at %s have Euphonic installed?\n%s'], ...
+    warning(['Couldn''t import Euphonic Python library. Has it been ' ...
+             'installed correctly for the currently loaded Python at %s?\n%s'], ...
              ex, ME.message);
 end
 
